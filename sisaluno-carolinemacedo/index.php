4ebc37f93c72be8e6a74b6ae1a0a7f6235b9a266
Caroline Monteiro <129870024+carolinemonteirox@users.noreply.github.com>
2023-07-20 23:44:52 -0300

    Add files via upload

---
+++ sisaluno-carolinemacedo/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+<div class="container">
+    <div class="form-image">
+        <img src="cadastro.jpg" alt="">
+    </div>
+
+    <div class="title">
+            <h1>SISTEMA DE ALUNOS<h1>
+        </div>
+
+
+    <div class="form-header">
+        
+        <button class="button"><a href="cadaluno.php">Cadastrar Aluno</a></button>
+        <button class="button"><a href="listaalunos.php">Ver Lista</a></button>
+  
+    </div>
+</div>
+
+    
+  
+   
+</body>
+</html>
